0a9f85e6b94c8ef981ede4949075870e8a40b29b
Ganeshghatti <ghattiganesh8@gmail.com>
2024-02-22 01:30:34 +0530

    changed backend structure

--- Admin/routes/Admin.js
+++ Admin/routes/Admin.js
@@ -18,19 +18,17 @@ const { CreateCourse, GetAllCourse } = require("../Controller/Admin");
 
 router.route("/admin/login").post(AdminLogin);
 
-router.use(requireAuth);
-
-router.route("/admin/create-course").post(CreateCourse);
-router.route("/admin/get-all-course").get(GetAllCourse);
-router.route("/admin/create-instructor").post(CreateInstructor);
-router.route("/admin/get-all-instructors").get(GetAllInstructors);
-router.route("/admin/get-all-course-names").get(GetAllCourseNames);
-router.route("/admin/get-all-category").get(GetAllCategory);
-router.route("/admin/edit-category").put(EditCategory);
-router.route("/admin/edit-instructor/:instructorId").patch(EditInstructor);
-router.route("/admin/delete-instructor/:instructorId").delete(DeleteInstructor);
-router.route("/admin/change-password").post(ChangePassword);
-router.route("/admin/get-formdata").get(GetFormData);
-router.route("/admin/get-all-users").get(GetAllUsers);
+router.route("/admin/create-course").post(requireAuth,CreateCourse);
+router.route("/admin/get-all-course").get(requireAuth,GetAllCourse);
+router.route("/admin/create-instructor").post(requireAuth,CreateInstructor);
+router.route("/admin/get-all-instructors").get(requireAuth,GetAllInstructors);
+router.route("/admin/get-all-course-names").get(requireAuth,GetAllCourseNames);
+router.route("/admin/get-all-category").get(requireAuth,GetAllCategory);
+router.route("/admin/edit-category").put(requireAuth,EditCategory);
+router.route("/admin/edit-instructor/:instructorId").patch(requireAuth,EditInstructor);
+router.route("/admin/delete-instructor/:instructorId").delete(requireAuth,DeleteInstructor);
+router.route("/admin/change-password").post(requireAuth,ChangePassword);
+router.route("/admin/get-formdata").get(requireAuth,GetFormData);
+router.route("/admin/get-all-users").get(requireAuth,GetAllUsers);
 
 module.exports = router;
