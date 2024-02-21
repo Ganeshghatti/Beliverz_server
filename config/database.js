14effafe16ebf548594cefb8f67609e7f21b0631
Ganeshghatti <ghattiganesh8@gmail.com>
2024-02-22 01:35:13 +0530

    changed backend structure

--- config/database.js
+++ config/database.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 
 const connectdatabase = async () => {
   try {
-    console.log(process.env.MONGODB_CONNECT_URI)
-    await mongoose.connect(process.env.MONGODB_CONNECT_URI);
+    await mongoose.connect("mongodb+srv://ganeshghatti6:bba9gXnE2rCfZRRl@beliverz.hebeekj.mongodb.net/?retryWrites=true&w=majority");
     console.log("connected")
   } catch (error) {
     console.log("Failed" + error.message);
